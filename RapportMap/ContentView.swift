7878ee9632815d6cbdafd76f61f3b2658b13d29a
Leeo <mizzking75@gmail.com>
2025-11-02 17:36:30 +0900

    Initial Commit

---
+++ RapportMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RapportMap
+//
+//  Created by hyunho lee on 11/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RapportMap/RapportMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapportMapApp.swift
+//  RapportMap
+//
+//  Created by hyunho lee on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RapportMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
