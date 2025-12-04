f3bdb4556d8a22a9746adea0cc04f1fb48475ba8
Leeo <mizzking75@gmail.com>
2025-12-04 15:43:56 +0900

    add mac target

---
+++ mac/macApp.swift
@@ -0,0 +1,17 @@
+//
+//  macApp.swift
+//  mac
+//
+//  Created by Leeo on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
